d2b3ad94a0aeb58e30dd25719a13c7e27648d103
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2022-06-19 12:17:32 -0400

    update

---
+++ doc/src/lecture8/tmp_missing_labels.sh
@@ -0,0 +1,4 @@
+#rm -rf tmp_mako__* tmp_preprocess__*
+find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{fig-ccd}" {} \;
+find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{fig-ccd}" {} \;
+find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{fig-ccd}" {} \;
